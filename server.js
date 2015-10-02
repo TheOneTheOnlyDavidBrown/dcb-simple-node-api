9ef13cfce30359a7966d9338c9706aae00607d77
David Brown <dcbrown07@gmail.com>
2015-10-02 18:57:17 -0400

    adding cors support

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
-var env = require('dotenv').load()
+var env = require('dotenv').load();
+var cors = require('cors');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -9,33 +10,68 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var port = process.env.PORT || 8080; // set our port
 var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   res.json({
     message: 'root response'
   });
 });
 
 router.route('/demo')
-  .post(function (req, res) {
+  .post(function(req, res) {
     res.json({
       message: 'response from demo via POST'
     });
   })
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.json({
       message: 'response from demo via GET'
     });
   });
 
-router.get('/not200',function(req, res){
+router.get('/not200', function(req, res) {
   res.status(400);
   res.send('sending status code 400');
+});
+
+router.get('/beer/:id', function(req, res) {
+  res.status(200);
+  response = {
+    id: 32112,
+    style: 'farmhouse',
+    abv: '7%'
+  }
+  res.send(response);
 })
+
+router.route('/beer')
+  .post(function(req, res) {
+    response = req.body;
+    console.log(req.body.id);
+    response.id = 123123123123;
+    res.send(response);
+  })
+  .get(function(req, res) {
+    response = [{
+      id: 98098,
+      style: 'lager',
+      abv: '6%'
+    }, {
+      id: 32112,
+      style: 'ale',
+      abv: '5%'
+    }, {
+      id: 32112,
+      style: 'farmhouse',
+      abv: '7%'
+    }]
+    res.send(response);
+  });
 
 
 // additional routes go here
